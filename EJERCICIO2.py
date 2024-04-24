856cd46306ab615996f44088e9385ff817225941
Jhasmin22194 <161260872+Jhasmin22194@users.noreply.github.com>
2024-04-24 19:30:34 -0400

    Update EJERCICIO2.py

--- EJERCICIO2.py
+++ EJERCICIO2.py
@@ -2,24 +2,36 @@
 de cada vector y mostrar sus factoriales. En el caso de que alguno de los vectores o los
 dos no tengan números primos mostrar el mensaje “Hasta luego!!!”'''
 def llenarVector(v,n):
-    print(f"Ingrese {n+1} valores")
+    print(f"Ingrese {n} valores")
     for i in range (n):
         v[i]=int(input(""))
     print(v)
 
-def mayor(v,n):
-    may=v[0]
-    for i in range(1,n):
-        if v[i]>may:
-            may=v[i]
-    return may
+def esprimo(n):
+    c=0
+    for j in range(1, n+1):
+        		if n%j==0:
+        			c+=1
+    return c==2
+  
+def factorial(n):
+    f = 1
+    for j in range(1, n + 1):
+        f *= j
+    return f
     
-def hallarp(v,n):
-    
-    for i in range (n):
-        for 
-    print(v)
-    may=mayor(v,n)
+def mayor(v):
+    may = v[0]
+    sw = 0
+    for i in v:
+        if esprimo(i):
+            sw = 1
+            if i > may:
+                may = i
+    if sw == 0:
+        print("Hasta luego!!!")
+    else:
+        print(f"Factorial de {may} = {factorial(may)}")
 
 """MAIN"""
 n=int(input("Ingrese el valor de N: "))
@@ -27,4 +39,5 @@ va=[0]*n
 vb=[0]*n
 llenarVector(va,n)
 llenarVector(vb,n)
-
+mayor(va)
+mayor(vb)
